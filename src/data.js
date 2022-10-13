2934a7e395ad51b32b2c842e6926ed4abfdb247c
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-10-12 23:42:17 -0600

    Creating data.js file to store the data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import SanSalvador from "./images/SanSalvador.JPG";
+import Hollywood from "./images/hollywood.JPG";
+import MET from "./images/met.jpg";
+
+export default [
+    {
+        id: 1,
+        locationImg: SanSalvador,
+        location: "SAN SALVADOR, ESA",
+        name: "Historic Center of San Salvador",
+        date: "25 Apr, 2015",
+        description: "The historic center of San Salvador comprises the area where the expansion of the capital city of El Salvador began in the 16th century. The original buildings of the Spanish colony were mostly destroyed by natural disasters over the years, and the notable buildings that survived were erected in the late 19th and early 20th centuries.",
+    },
+    {
+        id: 2,
+        locationImg: Hollywood,
+        location: "CALIFORNIA, USA",
+        name: "Hollywood",
+        date: "12 jan, 2016 - 13 jan, 2016",
+        description: "Hollywood is a neighborhood located in Los Angeles, California, that's also synonymous with the glamour, money and power of the entertainment industry. As the show-business capital of the world, Hollywood is home to many famous television and movie studios and record companies",
+    },
+    {
+        id: 2,
+        locationImg: MET,
+        location: "NEW YORK, USA",
+        name: "Metropolitan Museum of Art",
+        date: "12 jun, 2021",
+        description: "The Metropolitan Museum of Art of New York City, colloquially 'the Met', is the largest art museum in the Americas. Its permanent collection contains over two million works, divided among 17 curatorial departments.",
+    }
+]
