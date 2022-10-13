14830a6123bd1f384ddcd95dbdaa77912de7d85d
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-10-12 23:03:55 -0600

    Adding Card Location component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header"
-
+import CardLocation from "./components/CardLocation";
 
 
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
         <Header />
+        <CardLocation />
     </div>
   );
 }
