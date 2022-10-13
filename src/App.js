e976a311162439f693386d5c646856ad42d463ce
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-10-13 00:23:43 -0600

    Using props to render the data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from "./components/Header"
 import CardLocation from "./components/CardLocation";
-
+import data from "./data";
 
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <CardLocation 
+      key={item.id}
+      {...item}
+    />
+    )
+  });
+
   return (
     <div className="App">
         <Header />
-        <CardLocation />
+      <section className="cards--list">
+        {cards}
+      </section>
+        
     </div>
   );
 }
--- src/components/CardLocation.js
+++ src/components/CardLocation.js
@@ -1,32 +1,26 @@
 import React from "react"
-import MET from "../images/met.jpg"
 import LocationIcon from "../images/pin.png"
 
-export default function CardLocation() {
+export default function CardLocation(props) {
     return (
         <>
+        
             <div className="card--container">
-                <img src={MET} className="card--image" alt="location"/>
+                <img src={props.locationImg} className="card--image" alt="location"/>
                     <div className="card--info">
                         <div className="card--description">
                             <div className="card--location">
-                                <span><img src={LocationIcon} style={{width: '18px', height:'20px'}}/></span>
-                                <span><p className="location--name">NEW YORK </p></span>
-                                <span><a href="https://goo.gl/maps/Bmw7sAjBD1bBsZK89"> View on Google Maps</a></span>
+                                <span><img className="location--icon" src={LocationIcon} alt="location icon"/></span>
+                                <span><p className="location--name">{props.location}</p></span>
+                                <span><a href={props.link}> View on Google Maps</a></span>
                             </div>
-                            <h1 className="card--location-name">Metropolitan Museum of Art</h1>
-                                <p className="bold">12 jan, 2021 - 24 Jan, 2021</p>
-                                    <p>
-                                        The Metropolitan Museum of Art of New York City, colloquially 
-                                        "the Met", is the largest art museum in the Americas. 
-                                        Its permanent collection contains over two million works,
-                                        divided among 17 curatorial departments.
-                                    </p>
+                            <h1 className="card--location-name">{props.name}</h1>
+                                <p className="bold">{props.date}</p>
+                                    <p>{props.description}</p>
                         </div>
-                        
                     </div>
             </div>
-            <hr></hr>
+                <hr></hr>
         </>
     )
 }
